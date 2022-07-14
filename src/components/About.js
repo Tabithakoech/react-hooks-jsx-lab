945df030852131353cbeb5edbc34116bcc7e4672
TabithaKoech <tabbyk1718@gmail.com>
2022-07-14 13:58:10 +0300

    Done.

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,12 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return(
+    <div id="About">
+      <h2>About me</h2>
+      <p>I Love singing</p>
+      <img src={image} alt=""/>
+    </div>  )
 }
 
 export default About;
